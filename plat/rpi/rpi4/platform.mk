93708a947171baea99c68e30a9fab29faf5299e4
Jeremy Linton <jeremy.linton@arm.com>
2020-09-30 18:08:03 -0500

    rpi4: Add el3 cpu frequency trampoline
    The rpi4 has a unused(?) "ARM mailbox" interface that can
    be used as a generic memory region that generates interrupts
    when written. Wire up the first interrupt a group0/secure and
    when we detect it pick up the requested frequency and create
    a videocore mailbox call to adjust the frequency.
    
    The result of this, is that we can implement ACPI CPPC
    frequency scaling (actually it might be possible to do
    legacy pstate/etc too) on the rpi4.
    
    Signed-off-by: Jeremy Linton <jeremy.linton@arm.com>

--- plat/rpi/rpi4/platform.mk
+++ plat/rpi/rpi4/platform.mk
@@ -26,6 +26,8 @@ BL31_SOURCES		+=	lib/cpus/aarch64/cortex_a72.S		\
 				drivers/rpi3/gpio/rpi3_gpio.c		\
 				plat/common/plat_gicv2.c                \
 				plat/rpi/rpi4/rpi4_bl31_setup.c		\
+				drivers/rpi3/mailbox/rpi3_mbox.c	\
+				plat/rpi/rpi4/rpi_clock.c		\
 				plat/rpi/common/rpi3_pm.c		\
 				plat/common/plat_psci_common.c		\
 				plat/rpi/common/rpi3_topology.c		\
@@ -85,6 +87,9 @@ RPI3_RUNTIME_UART		:= 0
 
 # Use normal memory mapping for ROM, FIP, SRAM and DRAM
 RPI3_USE_UEFI_MAP		:= 0
+
+# try to route all ints to el3 we care about..
+GICV2_G0_FOR_EL3                := 1
 
 # Process platform flags
 # ----------------------
